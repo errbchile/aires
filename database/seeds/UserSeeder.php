cd5b3481a97bb9abea989ce2f8b38b5aa7f2c836
eromero <eromero@inteduc.cl>
2020-12-09 18:24:02 -0300

    se agrega UserSeeder y dos usuarios el admin y el usuario

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new User();
+        $user->name = "admin";
+        $user->email = "admin@admin.com";
+        $user->password = Hash::make('admin');
+        $user->profile = 1;
+        $user->save();
+
+        $user = new User();
+        $user->name = "Usuario";
+        $user->email = "usuario@usuario.com";
+        $user->password = Hash::make('usuario');
+        $user->profile = 2;
+        $user->save();
+    }
+}
